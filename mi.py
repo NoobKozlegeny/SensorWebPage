66d2843699a4213964e5100796310b6e5689e01c
Péter Tóth <toth00peter@gmail.com>
2022-04-08 21:23:24 +0200

    Can visualize now

--- mi.py
+++ mi.py
@@ -3,18 +3,45 @@ import base64
 import pandas as pd
 import pickle
 import pathlib
+
+from Measurement import Measurement
+
 def create_onedrive_directdownload (onedrive_link):
     data_bytes64 = base64.b64encode(bytes(onedrive_link, 'utf-8'))
     data_bytes64_String = data_bytes64.decode('utf-8').replace('/','_').replace('+','-').rstrip("=")
     resultUrl = f"https://api.onedrive.com/v1.0/shares/u!{data_bytes64_String}/root/content"
     return resultUrl
+
+# Visualizes accelerometer and gyroscope data
+def visualize_data ():
+    accelerometer_df = pd.read_csv(create_onedrive_directdownload(files[filenames.index(selected)]), skiprows=range(1,2))
+    gyro_df = pd.read_csv(create_onedrive_directdownload(files[filenames.index(selected) + 1]), skiprows=range(1,2))
+
+    st.write(f"This is {selected}'s data.")
+    st.line_chart(accelerometer_df)
+    st.write(f"This is {filenames[filenames.index(selected) + 1]}'s data.")
+    st.line_chart(gyro_df)
+    
+# def get_file(filename):
+#     if "ACCELEROMETER" in filename:
+#         return filter(lambda x: (x.accelerometer == filename), files)
+#     elif "GYROSCOPE" in filename:
+#         return filter(lambda x: (x.gyroscope == filename), files)
+
 first = "https://1drv.ms/u/s!Ao3uECOrt_zivFsWZaLIUT2MCYNd?e=3OSLQT"
 filenames = ["FastACCELEROMETER0328", "FastGYROSCOPE0328",
              "NormalACCELEROMETER0328", "NormalGYROSCOPE0328"]
 files = ["https://1drv.ms/u/s!Ao3uECOrt_zivFsWZaLIUT2MCYNd?e=3OSLQT", "https://1drv.ms/u/s!Ao3uECOrt_zivFzbxJbBqd6EHHzI?e=LdcSSy"
         , "https://1drv.ms/u/s!Ao3uECOrt_zivFqGnublBPKyWbg7?e=4OGr32", "https://1drv.ms/u/s!Ao3uECOrt_zivFmiE6GQ5w-PkeLG?e=aaB4lT"
         ]
+
+# files = [ Measurement("FastACCELEROMETER0328", "https://1drv.ms/u/s!Ao3uECOrt_zivFsWZaLIUT2MCYNd?e=3OSLQT",
+#                       "FastGYROSCOPE0328", "https://1drv.ms/u/s!Ao3uECOrt_zivFzbxJbBqd6EHHzI?e=LdcSSy"),
+#           Measurement("NormalACCELEROMETER0328", "https://1drv.ms/u/s!Ao3uECOrt_zivFqGnublBPKyWbg7?e=4OGr32",
+#                       "NormalGYROSCOPE0328", "https://1drv.ms/u/s!Ao3uECOrt_zivFmiE6GQ5w-PkeLG?e=aaB4lT")]
+
 st.sidebar.header('Data Selector')
+
 st.sidebar.subheader('Select or upload the data you would like to view here')
 uploaded_file = st.sidebar.file_uploader("Upload excel file", type=["xlsx", "csv"])
 
@@ -26,6 +53,7 @@ st.write("""
 You can view the data you uploaded or the data you selected from the list here
 
 """)
+
 st.write("""***""")
 
 if st.button('View uploaded'):
@@ -38,11 +66,15 @@ if st.button('View uploaded'):
             st.write(cdf)
     else: 
         st.write('No file was uploaded!')
+        
 st.write("""***""")
+
 if st.button('View selected'):
-    df = pd.read_excel(create_onedrive_directdownload(files[filenames.index(selected)]))
+    df = pd.read_csv(create_onedrive_directdownload(files[filenames.index(selected)]))
     st.write(df)
+    
 st.write("""***""")
+
 st.write("""Model loading""")
 filename = st.text_input("Enter file name")
 
@@ -50,6 +82,13 @@ if filename is not "":
     loaded_model = pickle.load(open(filename, 'rb'))
     st.write("Model loaded")
 
+#---------------------------------------------------------------------
 
+st.write("""***""")
+
+if st.button('Visualize data'):
+    visualize_data()
+    st.write("""***""")
+    
 
 
