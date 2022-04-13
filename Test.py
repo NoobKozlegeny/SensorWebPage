fc1c43b05b5ead8a926710076d749ffcd1564a08
Tóth Péter/NoobKozlegeny <toth00peter@gmail.com>
2022-04-13 17:51:30 +0200

    Made some work with pickle

--- Test.py
+++ Test.py
@@ -1,7 +1,21 @@
 import pickle
+from sklearn import datasets
+import pandas as pd
+import os
 
-dictToPickle = {'hello': 'world', 'kaga' : 'igen', 'teszt' : 'XDDD'}
+#Creating models to pickle later
+class Iris():
+    iris = datasets.load_iris(return_X_y=True, as_frame=True)
+    X = iris[0] #data
+    y = iris[1] #target
 
-pickle.dump(dictToPickle,
-            open('pickledDict.pickle', 'wb'),
+    print(X)
+    print(y)
+
+#Pickling models
+instIris = Iris()
+print(os.getcwd())
+
+pickle.dump(instIris,
+            open('iris.pickle', 'wb'),
             protocol=pickle.HIGHEST_PROTOCOL)
--- mi.py
+++ mi.py
@@ -1,10 +1,33 @@
 import streamlit as st
 import base64
 import pandas as pd
-import pickle as pl
+import pickle
 import pathlib
+from pathlib import Path
+import random
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+#%matplotlib inline
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedKFold
+from sklearn.datasets import load_digits
 
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import VotingClassifier
+
+from sklearn.metrics import accuracy_score
+
+#My classes
 from Measurement import Measurement
+from Test import Iris
 
 def create_onedrive_directdownload (onedrive_link):
     data_bytes64 = base64.b64encode(bytes(onedrive_link, 'utf-8'))
@@ -22,11 +45,33 @@ def visualize_data ():
     st.write(f"This is {filenames[filenames.index(selected) + 1]}'s data.")
     st.line_chart(gyro_df)
     
-# def get_file(filename):
-#     if "ACCELEROMETER" in filename:
-#         return filter(lambda x: (x.accelerometer == filename), files)
-#     elif "GYROSCOPE" in filename:
-#         return filter(lambda x: (x.gyroscope == filename), files)
+#Runs classifications
+def run_classifications(X, y):
+    #Classifications
+    model1 = DecisionTreeClassifier(random_state=2)
+    model2 = KNeighborsClassifier(n_neighbors=50)
+    model3 = LogisticRegression(random_state=2, max_iter=5000)
+    vc = VotingClassifier(estimators=[('lr', model1), ('rf', model2), ('gnb', model3)], voting='hard')
+
+    st.write("DecisionTreeClassifier:")
+    st.line_chart(pd.DataFrame(data=stacking(model1, X, y)))
+    st.write("KNeighborsClassifier:")
+    st.line_chart(pd.DataFrame(data=stacking(model2, X, y)))
+    st.write("LogisticRegression:")
+    st.line_chart(pd.DataFrame(data=stacking(model3, X, y)))
+    #stacking(model1, X, y)
+    
+    
+
+def stacking(model,X,y):
+    # split the Train dataset again
+    X_train2, X_test2, y_train2, y_test2 = train_test_split(X, y, test_size=0.2, random_state=2)
+    # fit the model on the NEW train dataset
+    model.fit(X_train2, y_train2)
+    # print the accuracy of the model on the NEW test dataset
+    print(model.score(X_test2, y_test2))
+    # return the model prediction of whole X dataset
+    return model.predict(X)
 
 first = "https://1drv.ms/u/s!Ao3uECOrt_zivFsWZaLIUT2MCYNd?e=3OSLQT"
 filenames = ["FastACCELEROMETER0328", "FastGYROSCOPE0328",
@@ -34,11 +79,6 @@ filenames = ["FastACCELEROMETER0328", "FastGYROSCOPE0328",
 files = ["https://1drv.ms/u/s!Ao3uECOrt_zivFsWZaLIUT2MCYNd?e=3OSLQT", "https://1drv.ms/u/s!Ao3uECOrt_zivFzbxJbBqd6EHHzI?e=LdcSSy"
         , "https://1drv.ms/u/s!Ao3uECOrt_zivFqGnublBPKyWbg7?e=4OGr32", "https://1drv.ms/u/s!Ao3uECOrt_zivFmiE6GQ5w-PkeLG?e=aaB4lT"
         ]
-
-# files = [ Measurement("FastACCELEROMETER0328", "https://1drv.ms/u/s!Ao3uECOrt_zivFsWZaLIUT2MCYNd?e=3OSLQT",
-#                       "FastGYROSCOPE0328", "https://1drv.ms/u/s!Ao3uECOrt_zivFzbxJbBqd6EHHzI?e=LdcSSy"),
-#           Measurement("NormalACCELEROMETER0328", "https://1drv.ms/u/s!Ao3uECOrt_zivFqGnublBPKyWbg7?e=4OGr32",
-#                       "NormalGYROSCOPE0328", "https://1drv.ms/u/s!Ao3uECOrt_zivFmiE6GQ5w-PkeLG?e=aaB4lT")]
 
 st.sidebar.header('Data Selector')
 
@@ -79,8 +119,12 @@ st.write("""Model loading""")
 filename = st.text_input("Enter file name")
 
 if filename is not "":
-    loaded_model = pickle.load(open(filename, 'rb'))
+    HERE = Path(__file__).parent
+    loaded_model = pickle.load(open(HERE / filename, 'rb'))
     st.write("Model loaded")
+    st.write(loaded_model)
+    run_classifications(loaded_model.X, loaded_model.y)
+
 
 #---------------------------------------------------------------------
 
