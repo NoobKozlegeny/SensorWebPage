d52ff25a875b670b2afe6b1df6532f8023ac82c2
Péter Tóth <toth00peter@gmail.com>
2022-04-18 18:52:56 +0200

    Changed classification

--- Test.py
+++ Test.py
@@ -2,15 +2,52 @@ import pickle
 from sklearn import datasets
 import pandas as pd
 import os
+import random
+import numpy as np
+from matplotlib import pyplot as plt
+#%matplotlib inline
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedKFold
+from sklearn.datasets import load_digits
+
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import VotingClassifier
+
+from sklearn.metrics import accuracy_score
 
 #Creating models to pickle later
+def stacking(model,X,y):
+    # split the Train dataset again
+    X_train2, X_test2, y_train2, y_test2 = train_test_split(X, y, test_size=0.2, random_state=2)
+    # fit the model on the NEW train dataset
+    model.fit(X_train2, y_train2)
+    # print the accuracy of the model on the NEW test dataset
+    print("accuracy:", model.score(X_test2, y_test2))
+    # # return the model prediction of whole X dataset
+    # return model.predict(X)
 class Iris():
     iris = datasets.load_iris(return_X_y=True, as_frame=True)
     X = iris[0] #data
     y = iris[1] #target
-
-    print(X)
-    print(y)
+    
+    #Classifications
+    model_DT = DecisionTreeClassifier(random_state=2)
+    model_KNN = KNeighborsClassifier(n_neighbors=50)
+    model_LR = LogisticRegression(random_state=2, max_iter=5000)
+    model_V = VotingClassifier(estimators=[('dtc', model_KNN), ('knn', model_DT), ('lr', model_LR)], voting='hard')
+    
+    #Fitting/training models
+    stacking(model_DT, X, y)
+    stacking(model_KNN, X, y)
+    stacking(model_LR, X, y)
+    stacking(model_V, X, y)
 
 #Pickling models
 instIris = Iris()
--- mi.py
+++ mi.py
@@ -3,31 +3,21 @@ import base64
 import pandas as pd
 import pickle
 import pathlib
-from pathlib import Path
 import random
 import numpy as np
 import pandas as pd
-from matplotlib import pyplot as plt
+
+from pathlib import Path
+from matplotlib import pyplot
 #%matplotlib inline
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import StratifiedKFold
-from sklearn.datasets import load_digits
-
-
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import VotingClassifier
-
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix, plot_confusion_matrix
 
 #My classes
 from Measurement import Measurement
 from Test import Iris
+
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
 def create_onedrive_directdownload (onedrive_link):
     data_bytes64 = base64.b64encode(bytes(onedrive_link, 'utf-8'))
@@ -39,39 +29,43 @@ def create_onedrive_directdownload (onedrive_link):
 def visualize_data ():
     accelerometer_df = pd.read_csv(create_onedrive_directdownload(files[filenames.index(selected)]), skiprows=range(1,2))
     gyro_df = pd.read_csv(create_onedrive_directdownload(files[filenames.index(selected) + 1]), skiprows=range(1,2))
-
     st.write(f"This is {selected}'s data.")
     st.line_chart(accelerometer_df)
     st.write(f"This is {filenames[filenames.index(selected) + 1]}'s data.")
     st.line_chart(gyro_df)
     
-#Runs classifications
-def run_classifications(X, y):
-    #Classifications
-    model1 = DecisionTreeClassifier(random_state=2)
-    model2 = KNeighborsClassifier(n_neighbors=50)
-    model3 = LogisticRegression(random_state=2, max_iter=5000)
-    vc = VotingClassifier(estimators=[('lr', model1), ('rf', model2), ('gnb', model3)], voting='hard')
+# #Runs classifications
+# def run_classifications(X, y):
+#     #Classifications
+#     model1 = DecisionTreeClassifier(random_state=2)
+#     model2 = KNeighborsClassifier(n_neighbors=50)
+#     model3 = LogisticRegression(random_state=2, max_iter=5000)
+#     vc = VotingClassifier(estimators=[('lr', model1), ('rf', model2), ('gnb', model3)], voting='hard')
 
-    st.write("DecisionTreeClassifier:")
-    st.line_chart(pd.DataFrame(data=stacking(model1, X, y)))
-    st.write("KNeighborsClassifier:")
-    st.line_chart(pd.DataFrame(data=stacking(model2, X, y)))
-    st.write("LogisticRegression:")
-    st.line_chart(pd.DataFrame(data=stacking(model3, X, y)))
-    #stacking(model1, X, y)
+#     st.write("DecisionTreeClassifier:")
+#     st.line_chart(pd.DataFrame(data=stacking(model1, X, y)))
+#     st.write(model1.predict(X))
+    
+#     st.write("KNeighborsClassifier:")
+#     st.line_chart(pd.DataFrame(data=stacking(model2, X, y)))
+#     st.write("DecisionTreeClassifier:")
+    
+#     st.write("LogisticRegression:")
+#     st.line_chart(pd.DataFrame(data=stacking(model3, X, y)))
+#     st.write("DecisionTreeClassifier:")
+#     #stacking(model1, X, y)
     
     
 
-def stacking(model,X,y):
-    # split the Train dataset again
-    X_train2, X_test2, y_train2, y_test2 = train_test_split(X, y, test_size=0.2, random_state=2)
-    # fit the model on the NEW train dataset
-    model.fit(X_train2, y_train2)
-    # print the accuracy of the model on the NEW test dataset
-    print(model.score(X_test2, y_test2))
-    # return the model prediction of whole X dataset
-    return model.predict(X)
+# def stacking(model,X,y):
+#     # split the Train dataset again
+#     X_train2, X_test2, y_train2, y_test2 = train_test_split(X, y, test_size=0.2, random_state=2)
+#     # fit the model on the NEW train dataset
+#     model.fit(X_train2, y_train2)
+#     # print the accuracy of the model on the NEW test dataset
+#     st.write("accuracy:", model.score(X_test2, y_test2))
+#     # return the model prediction of whole X dataset
+#     return model.predict(X)
 
 first = "https://1drv.ms/u/s!Ao3uECOrt_zivFsWZaLIUT2MCYNd?e=3OSLQT"
 filenames = ["FastACCELEROMETER0328", "FastGYROSCOPE0328",
@@ -122,9 +116,31 @@ if filename is not "":
     HERE = Path(__file__).parent
     loaded_model = pickle.load(open(HERE / filename, 'rb'))
     st.write("Model loaded")
-    st.write(loaded_model)
-    run_classifications(loaded_model.X, loaded_model.y)
-
+    
+    #Getting test data
+    X_train, X_test, y_train, y_test = train_test_split(loaded_model.X, loaded_model.y, test_size=0.2, random_state=2)
+    
+    #Running classification/predicting
+    pred_DT = loaded_model.model_DT.predict(X_test)
+    pred_KNN = loaded_model.model_KNN.predict(X_test)
+    pred_LR = loaded_model.model_LR.predict(X_test)
+    pred_V = loaded_model.model_V.predict(X_test)
+    st.write("DecisionTreeClassifier accuracy: ", accuracy_score(y_test, pred_DT))
+    #st.write(confusion_matrix(y_test, pred_DT))
+    plot_confusion_matrix(loaded_model.model_DT, X_test, y_test)
+    st.pyplot()
+    st.write("KNeighborsClassifier accuracy: ", accuracy_score(y_test, pred_KNN))
+    #st.write(confusion_matrix(y_test, pred_KNN))
+    plot_confusion_matrix(loaded_model.model_KNN, X_test, y_test)
+    st.pyplot()
+    st.write("LogisticRegression accuracy: ", accuracy_score(y_test, pred_LR))
+    #st.write(confusion_matrix(y_test, pred_LR))
+    plot_confusion_matrix(loaded_model.model_LR, X_test, y_test)
+    st.pyplot()
+    st.write("VotingClassifier accuracy: ", accuracy_score(y_test, pred_V))
+    #st.write(confusion_matrix(y_test, pred_V)) 
+    plot_confusion_matrix(loaded_model.model_V, X_test, y_test)
+    st.pyplot()  
 
 #---------------------------------------------------------------------
 
